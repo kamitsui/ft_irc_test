64f937c2a47d9dfe490fee81e035c4ba4d40fe4d
kamitsui <kamitsui@student.42tokyo.jp>
2025-10-25 03:05:25 +0900

    Refactor: Update unit test main.cpp for clarity
     - Add comments to explain the purpose of g_running in unit tests.
     - Comment out the signalHandler as it's not needed for unit test execution.

--- unit_tests/main.cpp
+++ unit_tests/main.cpp
@@ -5,12 +5,14 @@
 // コンパイル時に全てのテストソースファイルを含めることで、
 // GTestがそれらを自動的に発見します。
 
+// Server.o がリンク時に参照するために g_running の実体を定義する。
+// ユニットテストでは mainLoop() を実行しないため、
+// この値が false になることは想定しない（シグナルハンドラも不要）。
 volatile bool g_running = true;
-
-void signalHandler(int signum) {
-    (void)signum;
-    g_running = false;
-}
+// void signalHandler(int signum) {
+//     (void)signum;
+//     g_running = false;
+// }
 
 int main(int argc, char **argv) {
     // Initializes Google Test. This must be called before RUN_ALL_TESTS().
